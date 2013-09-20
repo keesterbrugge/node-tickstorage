6a4033f62ca7d169cdae47a7280cdec749378e97
Danial Bratchenko <bratchenko@gmail.com>
2013-09-20 16:04:28 +0300

    documentation arrived

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ node-tickstorage
 ================
 
 An extremely fast and efficient Node.js storage engine for raw stock market tick data.
+
+See documentation at [http://egorfine.github.io/node-tickstorage](egorfine.github.io/node-tickstorage).
--- index.js
+++ index.js
@@ -1,3 +1,69 @@
+/*
+Market tick storage for Node.js
+=============
+
+Stores and reads market ticks data in very condensed format and does it very fast.
+
+Writing ticks
+--------------
+Example usage:
+
+```javascript
+var Writer = require('tickstorage').Writer;
+
+writer = new Writer("/tmp/test.ticks");
+
+writer.addTick({
+	unixtime: Date.now(),
+	volume: 100,
+	price: 50000,
+	bid: 50100,
+	ask: 49900,
+	bidSize: 100000,
+	askSize: 100000,
+	isMarket: true
+});
+
+writer.save(fucntion(err) {
+	if (err) return console.log(err);
+
+	console.log("Ticks saved successfully");
+});
+```
+
+See [Writer documentation](lib/Writer.html) for implementation details.
+
+
+Reading ticks
+----------
+Example usage:
+
+```javascript
+var Reader = require('tickstorage').Reader;
+
+reader = new Reader("/tmp/test.ticks");
+
+reader.load(function(err) {
+	if (err) return console.error(err);
+
+	console.log("Ticks count: ", reader.length);
+
+	var tick, totalVolume = 0;
+	while(tick = reader.nextTick()) {
+		if (tick.isMarket) {
+			totalVolume += tick.volume;
+		}
+	}
+
+	console.log("Total volume: ", totalVolume);
+});
+```
+
+Reader supports tick files created by [stock module](http://github.com/egorFiNE/node-stock).
+
+See [Reader documentation](lib/Reader.html) for implementation details.
+
+*/
 var
 	Reader = require('./lib/Reader'),
 	Writer = require('./lib/Writer');
